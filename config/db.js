34663dcc66c822dc04c05dac5ff37b0c54307e8f
Daniel Farah <danielfarah54@gmail.com>
2021-07-20 14:44:14 -0300

    removing unnecessary comments

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-// mongosh "mongodb+srv://cluster0.ow7fd.mongodb.net/myFirstDatabase" --username danielfarah54
 if(process.env.NODE_ENV == 'production') {
     module.exports = {MONGODB_URI: process.env.MONGODB_URI}
 }
