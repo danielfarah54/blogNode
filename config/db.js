fed6d18fec177f3d4dab5e0736740e4d0a5d9b8e
Daniel Farah <danielfarah54@gmail.com>
2021-07-20 12:43:37 -0300

    db update

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
+// mongosh "mongodb+srv://cluster0.ow7fd.mongodb.net/myFirstDatabase" --username danielfarah54
 if(process.env.NODE_ENV == 'production') {
-    module.exports = {mongoURI: 'mongodb+srv://danielfarah54:1029384756@cluster0.ow7fd.mongodb.net/blogapp-prod?retryWrites=true&w=majority'}
+    module.exports = {mongoURI: 'mongodb+srv://cluster0.ow7fd.mongodb.net/myFirstDatabase" --username danielfarah54 --password 1029384756'}
 }
 else {
     module.exports = {mongoURI: 'mongodb://localhost/blogapp'}
